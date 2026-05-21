cfb176952016b0ece8296418b853828b7a22e6d0
Bhhgtr <himandithb@gmail.com>
2026-05-21 15:51:14 +0530

    feat: add .dockerignore file and update server listen address in bin.ts

--- src/bin.ts
+++ src/bin.ts
@@ -1,7 +1,7 @@
-import { app } from "./index.js";
+import { app } from './index.js';
 
 const port = 3000;
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
